0981d0b8ba5d11cb25f35de329a281953edc7270
Vitalii Klymenko <Vitalii.Klymenko78@gmail.com>
2025-12-07 17:45:15 +0100

    correct err

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,36 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
 
 
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ftp.goit.study',
-      },
-    ],
-  },
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'ftp.goit.study',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+
+// // next.config.ts
+// const nextConfig = {
+//   reactStrictMode: true,
+//   experimental: {
+//     cveMitigation: true
+//   }
+// };
+
+// export default nextConfig;
+
+
+// next.config.ts
+const nextConfig = {
+  reactStrictMode: true
 };
 
 export default nextConfig;
-
