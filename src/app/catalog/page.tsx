1f5182be1f602664177bc7e0357cfabbe33336fa
Vitalii Klymenko <Vitalii.Klymenko78@gmail.com>
2026-01-28 23:49:42 +0100

    Added tablet and mobile layouts

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,24 +1,3 @@
-// // app/catalog/page.tsx
-
-// import VehicleFilters from '@/components/VehicleFilters/VehicleFilters';
-// import VehicleGrid from '@/components/VehicleGrid/VehicleGrid';
-// import styles from './catalog.module.css';
-
-// export default function CatalogPage() {
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.container}>
-//         <div className={styles.filtersColumn}>
-//           <VehicleFilters />
-//         </div>
-//         <div className={styles.listColumn}>
-//           <VehicleGrid />
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
-
 
 import VehicleFilters from '@/components/VehicleFilters/VehicleFilters';
 import VehicleGrid from '@/components/VehicleGrid/VehicleGrid';
